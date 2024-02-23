65650b39859af4e92b48e98e257b0ec48d9db05b
Aashish Anand <anandaashish512@gmail.com>
2024-02-23 12:37:44 +0530

    fuzzy-auth-canvas-setup

---
+++ components/ui/auth/loading.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+export const Loading=()=>{
+return (
+    <div className='h-full w-full flex-col  justify-center item-center'>
+        <Image
+        src='/logo.svg'
+        alt="Logo"
+        width={120}
+        height={120}
+        className='animate-pulse duration-700'/>
+    </div>
+)
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://immortal-perch-29.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
